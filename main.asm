7fe45117857816fe7bf6ea25a08e5577dc07a682
Filipe <44625571+maldisco@users.noreply.github.com>
2022-04-01 18:05:20 -0300

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,17 +2,16 @@
 
 .include "macros.asm"
 .text
-	
-	li s11, MMIO_set	# carrega para s11 o endereço a armazenar o estado do teclado
+	li s11, MMIO_set	
+
 #poll_loop:			# início do loop de polling
 #	lw t1, (s11)		# carrega para t1 o estado do teclado
 #	beqz t1, poll_loop	# se for igual a 0 (nada digitado), volta ao loop
 #	li s0, MMIO_add		# carrega para s0 o endereço a armazenar a tecla digitada
 #	lw s0, (s0)		# carrega para s0 a tecla digitada
 	j animacao_2b
-	
-	li a7, 10
-	ecall
+				
+
 	
 .include "render.asm"
 .include "walk.asm"
