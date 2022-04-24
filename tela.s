3387c5a0dc8eca769088c6bc0dae14e1a3c4c065
Filipe <44625571+maldisco@users.noreply.github.com>
2022-04-24 10:02:28 -0300

    Backup

--- tela.s
+++ tela.s
@@ -15,33 +15,8 @@ TELA.TROCA:		li		t0, -1
 			li		t0, -5
 			beq 		a0, t0, TELA_3.PARA.TELA_4
 
-TELA_1.PARA.TELA_2:	la		t1, mapa.imagem.largura
-			li 		t2, T2.LARGURA
-			sh 		t2, 0(t1)
-		
-			la		t1, mapa.hitbox.largura 
-			li 		t2, T2.LARGURA
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.max.x
-			li 		t2, T2.MAX.X
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.min.x
-			li		t2, T2.MIN.X
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.max.y
-			li		t2, T2.MAX.Y
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.min.y
-			li		t2, T2.MIN.Y
-			sh		t2, 0(t1)
-			
-			la 		t0, mapa_hitbox
-			la 		t1, tela_2_hitboxes
-			sw		t1, 0(t0)
+# Configurações para mudar da tela 1 para 2
+TELA_1.PARA.TELA_2:	jal 		CONFIG.TELA_2			# Padrões
 			
 			la 		t0, mapa.x
 			li		t1, T2.X_INI
@@ -79,59 +54,10 @@ TELA_1.PARA.TELA_2:	la		t1, mapa.imagem.largura
 			la		t0, socando
 			sb		zero, (t0)
 			
-			mv 		s9, s7			# S9 = Tela atual, S7 = Tela 2
-			
-			# Renderiza mapa na frame 1
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li		a4, MAPA.LARGURA
-			frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-			
-			# Renderiza mapa na frame 0
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li 		a4, MAPA.LARGURA
-			other_frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-	
 			tail		LOOP_JOGO			
 
-TELA_2.PARA.TELA_1:	la		t1, mapa.imagem.largura
-			li 		t2, T1.LARGURA
-			sh 		t2, 0(t1)
-		
-			la		t1, mapa.hitbox.largura 
-			li 		t2, T1.LARGURA
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.max.x
-			li 		t2, T1.MAX.X
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.min.x
-			li		t2, T1.MIN.X
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.max.y
-			li		t2, T1.MAX.Y
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.min.y
-			li		t2, T1.MIN.Y
-			sh		t2, 0(t1)
-			
-			la 		t0, mapa_hitbox
-			la 		t1, tela_1_hitboxes
-			sw		t1, 0(t0)
+# Configurações para mudar da tela 2 para 1
+TELA_2.PARA.TELA_1:	jal		CONFIG.TELA_1
 			
 			la 		t0, mapa.x
 			li		t1, 212
@@ -167,58 +93,11 @@ TELA_2.PARA.TELA_1:	la		t1, mapa.imagem.largura
 			sb		zero, (t0)
 			
 			mv 		s9, s8			# S9 = Tela atual, S8 = Tela 1
-			
-			# Renderiza mapa na frame 1
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li		a4, MAPA.LARGURA
-			frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-			
-			# Renderiza mapa na frame 0
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li 		a4, MAPA.LARGURA
-			other_frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
 	
 			tail		LOOP_JOGO
 
-TELA_2.PARA.TELA_3:	la		t1, mapa.imagem.largura
-			li 		t2, T3.LARGURA
-			sh 		t2, 0(t1)
-		
-			la		t1, mapa.hitbox.largura 
-			li 		t2, T3.LARGURA
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.max.x
-			li 		t2, T3.MAX.X
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.min.x
-			li		t2, T3.MIN.X
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.max.y
-			li		t2, T3.MAX.Y
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.min.y
-			li		t2, T3.MIN.Y
-			sh		t2, 0(t1)
-			
-			la 		t0, mapa_hitbox
-			la 		t1, tela_3_hitboxes
-			sw		t1, 0(t0)
+# Configurações para mudar da tela 2 para 3
+TELA_2.PARA.TELA_3:	jal		CONFIG.TELA_3
 			
 			la 		t0, mapa.x
 			li		t1, T3.X_INI
@@ -255,57 +134,11 @@ TELA_2.PARA.TELA_3:	la		t1, mapa.imagem.largura
 			
 			mv 		s9, s6			# S9 = Tela atual, S6 = Tela 3
 			
-			# Renderiza mapa na frame 1
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T3.LARGURA
-			li		a4, MAPA.LARGURA
-			frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-			
-			# Renderiza mapa na frame 0
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T3.LARGURA
-			li 		a4, MAPA.LARGURA
-			other_frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
 			
 			tail		LOOP_JOGO
 
-TELA_3.PARA.TELA_2:	la		t1, mapa.imagem.largura
-			li 		t2, T2.LARGURA
-			sh 		t2, 0(t1)
-		
-			la		t1, mapa.hitbox.largura 
-			li 		t2, T2.LARGURA
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.max.x
-			li 		t2, T2.MAX.X
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.min.x
-			li		t2, T2.MIN.X
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.max.y
-			li		t2, T2.MAX.Y
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.min.y
-			li		t2, T2.MIN.Y
-			sh		t2, 0(t1)
-			
-			la 		t0, mapa_hitbox
-			la 		t1, tela_2_hitboxes
-			sw		t1, 0(t0)
+# Configurações para mudar da tela 3 para 2
+TELA_3.PARA.TELA_2:	jal 		CONFIG.TELA_2
 			
 			la 		t0, mapa.x
 			li		t1, 0
@@ -340,60 +173,10 @@ TELA_3.PARA.TELA_2:	la		t1, mapa.imagem.largura
 			la		t0, socando
 			sb		zero, (t0)
 			
-			mv 		s9, s7			# S9 = Tela atual, S7 = Tela 2
-			
-			# Renderiza mapa na frame 1
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li		a4, MAPA.LARGURA
-			frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-			
-			# Renderiza mapa na frame 0
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li 		a4, MAPA.LARGURA
-			other_frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-	
 			tail		LOOP_JOGO
 
-TELA_3.PARA.TELA_4:
-			la		t1, mapa.imagem.largura
-			li 		t2, T4.LARGURA
-			sh 		t2, 0(t1)
-		
-			la		t1, mapa.hitbox.largura 
-			li 		t2, T4.LARGURA
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.max.x
-			li 		t2, T4.MAX.X
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.min.x
-			li		t2, T4.MIN.X
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.max.y
-			li		t2, T4.MAX.Y
-			sh		t2, 0(t1)
-			
-			la 		t1, mapa.min.y
-			li		t2, T4.MIN.Y
-			sh		t2, 0(t1)
-			
-			la 		t0, mapa_hitbox
-			la 		t1, tela_4_hitboxes
-			sw		t1, 0(t0)
+# Configurações ao mudar da tela 3 para 4
+TELA_3.PARA.TELA_4:	jal		CONFIG.TELA_4
 			
 			la 		t0, mapa.x
 			li		t1, T4.X_INI
@@ -430,56 +213,11 @@ TELA_3.PARA.TELA_4:
 			
 			mv 		s9, s5			# S9 = Tela atual, S5 = Tela 4
 			
-			# Renderiza mapa na frame 1
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li		a4, MAPA.LARGURA
-			frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-			
-			# Renderiza mapa na frame 0
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T2.LARGURA
-			li 		a4, MAPA.LARGURA
-			other_frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
 	
 			tail		LOOP_JOGO
 
-config_tela_1:
-			la		t1, mapa.imagem.largura
-			li 		t2, T1.LARGURA
-			sh 		t2, 0(t1)
-	
-			la		t1, mapa.hitbox.largura 
-			li 		t2, T1.LARGURA
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.max.x
-			li 		t2, T1.MAX.X
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.min.x
-			li		t2, T1.MIN.X
-			sh		t2, 0(t1)
-	
-			la 		t1, mapa.max.y
-			sh		zero, 0(t1)
-	
-			la 		t1, mapa.min.y
-			sh		zero, 0(t1)
-	
-			la 		t0, mapa_hitbox
-			la 		t1, tela_1_hitboxes
-			sw		t1, 0(t0)
+# Configuracoes ao iniciar o jogo
+config_tela_1:		jal		CONFIG.TELA_1
 	
 			la 		t0, mapa.x
 			li		t1, T1.X_INI
@@ -491,28 +229,6 @@ config_tela_1:
 	
 			mv		s9, s8			# S9 = Tela atual e S8 = Tela 1
 	
-			# Renderiza mapa na frame 1
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T1.LARGURA
-			li		a4, MAPA.LARGURA
-			frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-	
-			# Renderiza mapa na frame 0
-			mv 		a0, s9
-			li 		a1, 0
-			li 		a2, 0
-			li 		a3, T1.LARGURA
-			li 		a4, MAPA.LARGURA
-			other_frame_address(a5)
-			offset_mapa(a6)
-			li 		a7, MAPA.ALTURA
-			jal 		RENDER
-	
 			# Inicia velocidade Y como 0
 			la 		t1, velocidadeY_alucard
 			fcvt.s.w	ft1, zero
@@ -521,5 +237,135 @@ config_tela_1:
 			# Dialogo inicial do jogo			
  			tail 		LOOP_JOGO
  
+# Constantes ao mudar para o mapa 1
+CONFIG.TELA_1:		mv		s9, s8				# S9 = Tela atual, S8 = tela 1
+			
+			la		t1, mapa.imagem.largura
+			li 		t2, T1.LARGURA
+			sh 		t2, 0(t1)
+		
+			la		t1, mapa.hitbox.largura 
+			li 		t2, T1.LARGURA
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.max.x
+			li 		t2, T1.MAX.X
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.min.x
+			li		t2, T1.MIN.X
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.max.y
+			li		t2, T1.MAX.Y
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.min.y
+			li		t2, T1.MIN.Y
+			sh		t2, 0(t1)
+			
+			la 		t0, mapa_hitbox
+			la 		t1, tela_1_hitboxes
+			sw		t1, 0(t0)
+ 			
+ 			ret
+ 			
+# Constantes ao mudar para o mapa 2
+CONFIG.TELA_2:		mv 		s9, s7				# S9 = Tela atual, S7 = Tela 2
 
+			la		t1, mapa.imagem.largura
+			li 		t2, T2.LARGURA
+			sh 		t2, 0(t1)
+		
+			la		t1, mapa.hitbox.largura 
+			li 		t2, T2.LARGURA
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.max.x
+			li 		t2, T2.MAX.X
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.min.x
+			li		t2, T2.MIN.X
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.max.y
+			li		t2, T2.MAX.Y
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.min.y
+			li		t2, T2.MIN.Y
+			sh		t2, 0(t1)
+			
+			la 		t0, mapa_hitbox
+			la 		t1, tela_2_hitboxes
+			sw		t1, 0(t0)
 
+			ret
+
+# Constantes ao mudar para o mapa 3
+CONFIG.TELA_3:		mv		s9, s6				# s9 = tela atual, s5 = tela 3
+			
+			la		t1, mapa.imagem.largura
+			li 		t2, T3.LARGURA
+			sh 		t2, 0(t1)
+		
+			la		t1, mapa.hitbox.largura 
+			li 		t2, T3.LARGURA
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.max.x
+			li 		t2, T3.MAX.X
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.min.x
+			li		t2, T3.MIN.X
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.max.y
+			li		t2, T3.MAX.Y
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.min.y
+			li		t2, T3.MIN.Y
+			sh		t2, 0(t1)
+			
+			la 		t0, mapa_hitbox
+			la 		t1, tela_3_hitboxes
+			sw		t1, 0(t0)
+			
+			ret
+
+# Constantes ao mudar para tela 4
+CONFIG.TELA_4:		mv		s9, s5				# S9 = tela atual, s5 = tela 4
+			
+			
+			la		t1, mapa.imagem.largura
+			li 		t2, T4.LARGURA
+			sh 		t2, 0(t1)
+		
+			la		t1, mapa.hitbox.largura 
+			li 		t2, T4.LARGURA
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.max.x
+			li 		t2, T4.MAX.X
+			sh		t2, 0(t1)
+	
+			la 		t1, mapa.min.x
+			li		t2, T4.MIN.X
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.max.y
+			li		t2, T4.MAX.Y
+			sh		t2, 0(t1)
+			
+			la 		t1, mapa.min.y
+			li		t2, T4.MIN.Y
+			sh		t2, 0(t1)
+			
+			la 		t0, mapa_hitbox
+			la 		t1, tela_4_hitboxes
+			sw		t1, 0(t0)
+			
+			ret
